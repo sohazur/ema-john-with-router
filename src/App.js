15f42aed1a7f46156dd0c122ee1ff7229d6b1aec
Sohazur <mdsohazurislam@gmail.com>
2022-02-06 03:10:19 +0400

    Fixed routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import './App.css';
-import Header from './components/Header/Header';
-import Shop from './components/Shop/Shop';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header/Header";
+import Inventory from "./components/Inventory/Inventory";
+import NotFound from "./components/NotFound/NotFound";
+import OrderReview from "./components/OrderReview/OrderReview";
+import Shop from "./components/Shop/Shop";
 
 function App() {
   return (
     <div>
       <Header></Header>
-      <Shop></Shop>
+      <Routes>
+        <Route exact path="/" element={<Shop></Shop>} />
+        <Route exact path="/shop" element={<Shop></Shop>} />
+        <Route exact path="/review" element={<OrderReview></OrderReview>} />
+        <Route exact path="/inventory" element={<Inventory></Inventory>} />
+        <Route exact path="*" element={<NotFound></NotFound>} />
+      </Routes>
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,19 @@
-import React from 'react';
-import logo from '../../images/logo.png';
-import './Header.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../../images/logo.png";
+import "./Header.css";
 
 const Header = () => {
-    return (
-        <div className="header">
-            <img className="logo" src={logo} alt="" />
-            <nav>
-                <a href="/shop">Shop</a>
-                <a href="/orders">Order Review</a>
-                <a href="/inventory">Manage Inventory</a>
-            </nav>
-        </div>
-    );
+  return (
+    <div className="header">
+      <img className="logo" src={logo} alt="" />
+      <nav>
+        <Link to="/shop">Shop</Link>
+        <Link to="/review">Order Review</Link>
+        <Link to="/inventory">Manage Inventory</Link>
+      </nav>
+    </div>
+  );
 };
 
 export default Header;
