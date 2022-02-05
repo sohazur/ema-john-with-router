1e7bcbf4a0c412451a50cb2e183ff620957a850a
Sohazur <mdsohazurislam@gmail.com>
2022-02-06 03:35:12 +0400

    Added useCart

--- src/components/OrderReview/OrderReview.js
+++ src/components/OrderReview/OrderReview.js
@@ -1,8 +1,14 @@
 import React from "react";
+import useCart from "../../hooks/useCart";
+import useProducts from "../../hooks/useProducts";
 
 const OrderReview = () => {
+  const [products] = useProducts();
+  const [cart] = useCart(products);
   return (
     <div>
+      <h1>{products.length}</h1>
+      <h3>{cart.length}</h3>
       <h2>This is Order Review</h2>
     </div>
   );
