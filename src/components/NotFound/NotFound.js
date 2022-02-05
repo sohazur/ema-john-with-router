22d4a0eeefb8a110aa944172ad772a82b9d41825
Sohazur <mdsohazurislam@gmail.com>
2022-02-06 02:56:46 +0400

    Added not found page

---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div>
+      <h1>404</h1>
+      <h3>The page doesn't exist</h3>
+    </div>
+  );
+};
+
+export default NotFound;
