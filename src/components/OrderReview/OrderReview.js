beeb89c7e0fb4b8b0b84d2441771a45c7365443a
Sohazur <mdsohazurislam@gmail.com>
2022-02-06 03:54:26 +0400

    Added items in the order remove

--- src/components/OrderReview/OrderReview.js
+++ src/components/OrderReview/OrderReview.js
@@ -2,13 +2,25 @@ import React from "react";
 import useCart from "../../hooks/useCart";
 import useProducts from "../../hooks/useProducts";
 import Cart from "../Cart/Cart";
+import ReviewItem from "../ReviewItem/ReviewItem";
 
 const OrderReview = () => {
   const [products] = useProducts();
   const [cart] = useCart(products);
+  const handleRemove = (key) => {
+    console.log(key);
+  };
   return (
     <div className="shop-container">
-      <div className="product-container"></div>
+      <div className="product-container">
+        {cart.map((product) => (
+          <ReviewItem
+            key={product.key}
+            product={product}
+            handleRemove={handleRemove}
+          ></ReviewItem>
+        ))}
+      </div>
       <div className="cart-container">
         <Cart cart={cart}></Cart>
       </div>
---
+++ src/components/ReviewItem/ReviewItem.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const ReviewItem = (props) => {
+  const { name, price, quantity, img, key } = props.product;
+  const { handleRemove } = props;
+  return (
+    <div className="product">
+      <div>
+        <img src={img} alt={name} />
+      </div>
+      <div>
+        <h4 className="product-name">{name}</h4>
+        <p>Price: {(price * quantity).toFixed(2)}</p>
+        <p>Quantity: {quantity}</p>
+        <button onClick={() => handleRemove(key)} className="btn-regular">
+          Remove
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ReviewItem;
